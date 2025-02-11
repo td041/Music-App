12ebfa8fcb5997c09af573f717d36ae703487f35
td041 <trinhtrantrungduc@gmail.com>
2025-02-11 18:02:02 +0700

    New Changes

--- src/app/(pages)/songs/[id]/page.tsx
+++ src/app/(pages)/songs/[id]/page.tsx
@@ -7,7 +7,45 @@ export const metadata: Metadata = {
   title: "Chi tiết bài hát",
   description: "Online Music App",
 };
-export default function SongDetailPage() {
+export default async function SongDetailPage(props: any) {
+  const { id } = await props.params;
+  // Data Final
+  let dataFinal: any = null;
+  onValue(ref(dbFirebase, "/songs/" + id), (items) => {
+    dataFinal = items.val();
+    const singersRef = ref(dbFirebase, "/singers/" + dataFinal.singerId[0]);
+    onValue(singersRef, (itemSinger) => {
+      const dataSinger = itemSinger.val();
+      dataSinger["singer"] = dataSinger.title;
+    });
+  });
+  // End Data Final
+  // Data Section 3
+  const dataSection3: any[] = [];
+
+  const songRef = ref(dbFirebase, "songs");
+  onValue(songRef, (items) => {
+    items.forEach((item) => {
+      const key = item.key;
+      const data = item.val();
+      if (data.categoryId === dataFinal.categoryId) {
+        onValue(
+          ref(dbFirebase, "/singers/" + data.singerId[0]),
+          (itemSinger) => {
+            const dataSinger = itemSinger.val();
+            dataSection3.push({
+              id: key,
+              image: data.image,
+              title: data.title,
+              singer: dataSinger.title,
+              link: `/songs/${key}`,
+              time: "4:32",
+            });
+          }
+        );
+      }
+    });
+  });
   const lyrics = ` Verse: 
     Níu ngàn lời cũng không ngăn được   
     //...
